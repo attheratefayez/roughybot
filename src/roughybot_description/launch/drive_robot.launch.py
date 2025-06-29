7d3fdca23b3604cbe5c60c333a7e467d44d30bdc
attheratefayez <faizurrahman.fayez@gmail.com>
2025-06-30 02:30:10 +0600

    teleop_twist_keyboard not working from a launch file

--- src/roughybot_description/launch/drive_robot.launch.py
+++ src/roughybot_description/launch/drive_robot.launch.py
@@ -1,1 +1,22 @@
-# TODO: write launch file to launch teleop_twist_keyboard
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+#
+# def generate_launch_description():
+#
+#     teleop_twist_keyboard_node = Node(
+#         package="teleop_twist_keyboard",
+#         executable="teleop_twist_keyboard",
+#         output="screen",
+#     )
+#
+#     return LaunchDescription(
+#         [
+#             teleop_twist_keyboard_node
+#         ]
+#     )
+
+#
+# USE THIS COMMAND TO DRIVE THE ROBOT
+#
+# ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --param stamped:="true" --remap /cmd_vel:=/diff_drive_base_controller/cmd_vel
+
